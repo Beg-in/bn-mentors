21a5aa0b5b019ad6e6c148575eefc8caeed55178
Cody Sherman <meatwaddancin@gmail.com>
2016-01-15 12:35:40 -0600

    Angular routing working

---
+++ src/client/scripts/controllers/login.controller.js
@@ -0,0 +1,3 @@
+angular.module('bnMentorsApp').controller('loginController', function(
+) { 'use strict';
+});
---
+++ src/client/scripts/controllers/static.controller.js
@@ -0,0 +1,3 @@
+angular.module('bnMentorsApp').controller('staticController', function(
+) { 'use strict';
+});
