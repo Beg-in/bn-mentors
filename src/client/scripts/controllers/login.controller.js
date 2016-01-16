03f88f74cf37f05c66b33f4f8e8eb369db720a09
Ryan Greene <rabrg96@gmail.com>
2016-01-16 13:29:48 -0600

    Added functionality to login controller

--- src/client/scripts/controllers/login.controller.js
+++ src/client/scripts/controllers/login.controller.js
@@ -1,3 +1,19 @@
 angular.module('bnMentorsApp').controller('loginController', function(
+    $scope
 ) { 'use strict';
+
+    $scope.form = {
+        email: "",
+        password: ""
+    };
+
+    $scope.profile = {
+        email: "",
+        password: ""
+    };
+
+    $scope.sendFormToProfile = function() {
+        $scope.profile.email = $scope.form.email;
+        $scope.profile.password = $scope.form.password;
+    }
 });
