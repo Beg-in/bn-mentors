3158d7d65ae267fe43144fcf92428640aed23eef
Cody Sherman <meatwaddancin@gmail.com>
2016-01-19 17:44:56 -0600

    Gravatar improvements

--- src/client/scripts/controllers/profile.controller.js
+++ src/client/scripts/controllers/profile.controller.js
@@ -1,4 +1,5 @@
 angular.module('bnMentorsApp').controller('profileController', function(
+    $http,
     $scope,
     $routeParams,
     $location,
@@ -10,7 +11,7 @@ angular.module('bnMentorsApp').controller('profileController', function(
 
     $scope.profile = {
         name: 'Brandon Frisch',
-        email: 'bfrish1@gmail.com',
+        email: 'bfrisch1@gmail.com',
         phone: '847.847.8847',
         skills: ['law', 'business', 'technology', 'science'],
         shortbio: 'Hi, welcome to Chili\'s!'
@@ -25,17 +26,28 @@ angular.module('bnMentorsApp').controller('profileController', function(
         // $location.path('/#/404');
     }
 
+
+    $scope.fetchGravatar = function() {
+        $scope.gravatar = md5.createHash($scope.profile.email);
+        $http.get('https://secure.gravatar.com/avatar/' + $scope.gravatar + '?s=1&d=404')
+            .success(function(data, status) {
+                $scope.avatar = 'https://secure.gravatar.com/avatar/' + $scope.gravatar + '?s=500';
+            })
+            .error(function(data, status) {
+                $scope.avatar = null;
+            })
+        ;
+    }
+
     $scope.toggleEdit = function() {
         $scope.edit = !$scope.edit;
         if ($scope.profile.email) {
-            $scope.gravatar = md5.createHash($scope.profile.email);
+            $scope.fetchGravatar();
         }
     };
 
-    
-
     if ($scope.profile.email) {
-        $scope.gravatar = md5.createHash($scope.profile.email);
+        $scope.fetchGravatar();
     }
-    
+
 });
