578c95f0644cdd59da7fda7302fe4113dbd84162
Cody Sherman <meatwaddancin@gmail.com>
2016-01-19 23:04:29 -0600

    Added fake search results

--- src/client/scripts/controllers/search.controller.js
+++ src/client/scripts/controllers/search.controller.js
@@ -1,3 +1,20 @@
 angular.module('bnMentorsApp').controller('searchController', function(
+    $scope
 ) { 'use strict';
+
+    $scope.searchResults = [
+        {
+            name: "Radical Larry",
+            email: "test@gmale.com"
+        },
+        {
+            name: "James Bond",
+            email: "007@gmale.com"
+        },
+        {
+            name: "Brandon Frisch",
+            email: "befresh1@gmale.com"
+        }
+    ]
+
 });
