4335074c8ca4022f576e2c3a3da69838e6894cc2
Brandon <bfrisch1@gmail.com>
2016-01-20 00:56:47 -0600

    updated search page design

--- src/client/scripts/controllers/search.controller.js
+++ src/client/scripts/controllers/search.controller.js
@@ -4,16 +4,22 @@ angular.module('bnMentorsApp').controller('searchController', function(
 
     $scope.searchResults = [
         {
-            name: "Radical Larry",
-            email: "test@gmale.com"
+            name: 'Radical Larry',
+            bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            skills: ['cooking', 'burrowing', 'vibrating', 'business'],
+            jobTitle: 'All business all the time'
         },
         {
-            name: "James Bond",
-            email: "007@gmale.com"
+            name: 'James Bond',
+            bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            skills: ['shooting', 'women', 'spying', 'martinis'],
+            jobTitle: 'License to kill'
         },
         {
-            name: "Brandon Frisch",
-            email: "befresh1@gmale.com"
+            name: 'Brandon Frisch',
+            bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            skills: ['being awesome','coding', 'CSS', 'Startups'],
+            jobTitle: 'King Freak'
         }
     ]
 
