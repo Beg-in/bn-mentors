a4cbaa2026bb340e52603cb145982d9c8aee4843
Arzeimuth <bburton.cg@gmail.com>
2016-01-19 22:08:06 -0600

    Added beginning of login

--- src/server/controllers/login.controller.js
+++ src/server/controllers/login.controller.js
@@ -2,6 +2,10 @@
 
 module.exports = function(loginModel) {
     return {
-
+    	login: function(username, password) {
+    		return loginModel.loginUser().then(function(result) {
+    			return result.rows;
+    		});
+    	}
     };
 };
