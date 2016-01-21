22cd76523ec72ba0a2ccbbfdbe55b929af4f854e
Cody Sherman <meatwaddancin@gmail.com>
2016-01-20 23:19:18 -0600

    Altered example users

--- src/client/scripts/controllers/search.controller.js
+++ src/client/scripts/controllers/search.controller.js
@@ -4,23 +4,22 @@ angular.module('bnMentorsApp').controller('searchController', function(
 
     $scope.searchResults = [
         {
-            name: 'Radical Larry',
+            name: 'Henry Jones',
             shortBio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            skills: ['cooking', 'burrowing', 'vibrating', 'business'],
-            jobTitle: 'All business all the time'
+            skills: ['teaching', 'raiding', 'whipping', 'exploring'],
+            jobTitle: 'Archaeologist'
         },
         {
-            name: 'James Bond',
+            name: 'Rick Deckard',
             shortBio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            skills: ['shooting', 'women', 'spying', 'martinis'],
-            jobTitle: 'License to kill'
+            skills: ['dreaming','testing', 'retiring', 'running'],
+            jobTitle: 'Blade Runner'
         },
         {
-            name: 'Brandon Frisch',
+            name: 'Han Solo',
             shortBio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            skills: ['being awesome','coding', 'CSS', 'Startups'],
-            jobTitle: 'King Freak'
+            skills: ['flying', 'charming', 'friendship', 'nerf-herding'],
+            jobTitle: 'Pilot'
         }
     ]
-
 });
